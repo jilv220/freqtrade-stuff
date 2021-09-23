1f80dec39bb3010233eb74218bbe390becae39cf
jilv220 <LyuJi220@hotmail.com>
2021-09-23 17:49:14 -0400

    Add files via upload

--- pmaxTest.py
+++ pmaxTest.py
@@ -1,15 +1,18 @@
 # --- Do not remove these libs ---
 from freqtrade.strategy.interface import IStrategy
 from typing import Dict, List
+import numpy as np
 from functools import reduce
 from pandas import DataFrame, Series
-from technical.indicators import zema, VIDYA
+from technical.indicators import zema, VIDYA, RMI
+from datetime import datetime, timedelta
+from freqtrade.strategy import merge_informative_pair, CategoricalParameter, DecimalParameter, IntParameter, stoploss_from_open
 # --------------------------------
 
 import talib.abstract as ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 
-class PmaxTest(IStrategy):
+class pmaxTest(IStrategy):
     """
 
     author@: Gert Wohlgemuth
@@ -22,14 +25,68 @@ class PmaxTest(IStrategy):
     # adjust based on market conditions. We would recommend to keep it low for quick turn arounds
     # This attribute will be overridden if the config file contains "minimal_roi"
     minimal_roi = {
-        "0": 100   #disable roi
+        "0": 100,
+        #"0": 0.053,
+        #"23": 0.039,
+        #"62": 0.022,
+        #"186": 0
+    }
+
+    # sell space
+    sell_params = {
+        "pHSL": -0.04,
+        "pPF_1": 0.011,
+        "pPF_2": 0.069,
+        "pSL_1": 0.011,
+        "pSL_2": 0.068,
     }
 
     # Optimal stoploss designed for the strategy
-    stoploss = -0.25
+    stoploss = -0.99
+    use_custom_stoploss = True
 
     # Optimal timeframe for the strategy
     timeframe = '5m'
+
+     ## Trailing params
+
+    # hard stoploss profit
+    pHSL = DecimalParameter(-0.200, -0.040, default=-0.08, decimals=3, space='sell', load=True)
+    # profit threshold 1, trigger point, SL_1 is used
+    pPF_1 = DecimalParameter(0.008, 0.020, default=0.016, decimals=3, space='sell', load=True)
+    pSL_1 = DecimalParameter(0.008, 0.020, default=0.011, decimals=3, space='sell', load=True)
+
+    # profit threshold 2, SL_2 is used
+    pPF_2 = DecimalParameter(0.040, 0.100, default=0.080, decimals=3, space='sell', load=True)
+    pSL_2 = DecimalParameter(0.020, 0.070, default=0.040, decimals=3, space='sell', load=True)
+
+    ## Custom Trailing stoploss ( credit to Perkmeister for this custom stoploss to help the strategy ride a green candle )
+    def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
+                        current_rate: float, current_profit: float, **kwargs) -> float:
+
+        # hard stoploss profit
+        HSL = self.pHSL.value
+        PF_1 = self.pPF_1.value
+        SL_1 = self.pSL_1.value
+        PF_2 = self.pPF_2.value
+        SL_2 = self.pSL_2.value
+
+        # For profits between PF_1 and PF_2 the stoploss (sl_profit) used is linearly interpolated
+        # between the values of SL_1 and SL_2. For all profits above PL_2 the sl_profit value
+        # rises linearly with current profit, for profits below PF_1 the hard stoploss profit is used.
+
+        if (current_profit > PF_2):
+            sl_profit = SL_2 + (current_profit - PF_2)
+        elif (current_profit > PF_1):
+            sl_profit = SL_1 + ((current_profit - PF_1) * (SL_2 - SL_1) / (PF_2 - PF_1))
+        else:
+            sl_profit = HSL
+
+        # For hyperopt only
+        if (sl_profit >= current_profit):
+            return -0.99
+
+        return stoploss_from_open(sl_profit, current_profit)
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
@@ -42,28 +99,51 @@ class PmaxTest(IStrategy):
         dataframe['source'] = (dataframe['high'] + dataframe['low'] + dataframe['open'] + dataframe['close'])/4
         dataframe['pmax_thresh'] = ta.EMA(dataframe['source'], timeperiod=9)
 
+         # RSI
+        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
+        dataframe['rsi_fast'] = ta.RSI(dataframe, timeperiod=4)
+        dataframe['rsi_84'] = ta.RSI(dataframe, timeperiod=84)
+        dataframe['rsi_112'] = ta.RSI(dataframe, timeperiod=112)
+
         # trima
         dataframe['trima_buy'] = ta.TRIMA(dataframe, 10)
 
         # zema
         dataframe['zema_buy'] = zema(dataframe, 30)
 
+        dataframe['rmi'] = RMI(dataframe, length=9, mom=4)
+        dataframe['cci'] = ta.CCI(dataframe, 46)
+
+        stoch = ta.STOCHRSI(dataframe, 15, 20, 2, 2)
+        dataframe['srsi_fk'] = stoch['fastk']
+        dataframe['srsi_fd'] = stoch['fastd']
+
         return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (   
-                qtpylib.crossed_above('trima_buy', 'zema_buy') &
+                qtpylib.crossed_above(dataframe['trima_buy'], dataframe['zema_buy']) &
                 (dataframe['trima_buy'] > dataframe['pm']) &
-                (dataframe['zema_buy'] > dataframe['pm']) 
+                (dataframe['zema_buy'] > dataframe['pm']) &
+                # add check
+                (dataframe['rmi'] < 50) &
+                (dataframe['cci'] <= -91) &
+                (dataframe['srsi_fk'] < 41) &
+                (dataframe['close'].rolling(288).max() >= (dataframe['close'] * 1.10 )) &
+                (dataframe['rsi_fast'] < 35) &
+                (dataframe['rsi_84'] < 60) &
+                (dataframe['rsi_112'] < 60) &
+                (dataframe['volume'] > 0)
             ) 
             ,'buy'] = 1
         return dataframe
 
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
-            (qtpylib.crossed_below(dataframe['wt1'], dataframe['wt2']))
-            ,'sell'] = 0
+            (
+                (dataframe['volume'] > 0) # Make sure Volume is not 0
+            ),'sell'] = 0
         return dataframe
     
 
