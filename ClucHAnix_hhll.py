e62f4a84f1a380908adef78ea6545457cdb03b4d
jilv220 <LyuJi220@hotmail.com>
2021-11-29 20:56:11 -0500

    Add files via upload

--- ClucHAnix_hhll.py
+++ ClucHAnix_hhll.py
@@ -31,6 +31,9 @@ class ClucHAnix_hhll(IStrategy):
     
     #hypered params
     buy_params = {
+        ##
+        "max_slip": 0.73,
+        ##
         "bbdelta_close": 0.01846,
         "bbdelta_tail": 0.98973,
         "close_bblower": 0.00785,
@@ -49,10 +52,10 @@ class ClucHAnix_hhll(IStrategy):
         "pSL_2": 0.062,
 
         # sell signal params
-        'sell_fisher': 0.39075,
-        'sell_bbmiddle_close': 0.99754,
-        "high_offset": 0.991,
-        "high_offset_2": 0.997
+        "high_offset": 0.907,
+        "high_offset_2": 1.211,
+        "sell_bbmiddle_close": 0.97286,
+        "sell_fisher": 0.48492,
     }
 
     # ROI table:
@@ -112,23 +115,26 @@ class ClucHAnix_hhll(IStrategy):
     bbdelta_tail = RealParameter(0.7, 1.0, default=0.95089, space='buy', optimize = is_optimize_clucHA )
     close_bblower = RealParameter(0.0005, 0.02, default=0.00799, space='buy', optimize = is_optimize_clucHA )
 
-    is_optimize_hh_ll = True
+    is_optimize_hh_ll = False
     buy_hh_diff_48 = DecimalParameter(0.0, 15, default=1.087 , optimize = is_optimize_hh_ll )
     buy_ll_diff_48 = DecimalParameter(-23, 40, default=1.087 , optimize = is_optimize_hh_ll )
 
+    ## Slippage params
+    is_optimize_slip = False
+    max_slip = DecimalParameter(0.33, 0.80, default=0.33, decimals=3, optimize=is_optimize_slip , space='buy', load=True)
+
     # sell params
-    sell_fisher = RealParameter(0.1, 0.5, default=0.38414, space='sell', optimize=True)
-    sell_bbmiddle_close = RealParameter(0.97, 1.1, default=1.07634, space='sell', optimize=True)
-    high_offset          = DecimalParameter(0.95, 1.1, default=sell_params['high_offset'], space='sell', optimize=True)
-    high_offset_2        = DecimalParameter(0.99, 1.5, default=sell_params['high_offset_2'], space='sell', optimize=True)
+    is_optimize_sell = False
+    sell_fisher = RealParameter(0.1, 0.5, default=0.38414, space='sell', optimize = is_optimize_sell)
+    sell_bbmiddle_close = RealParameter(0.97, 1.1, default=1.07634, space='sell', optimize = is_optimize_sell)
+    high_offset          = DecimalParameter(0.90, 1.2, default=sell_params['high_offset'], space='sell', optimize = is_optimize_sell)
+    high_offset_2        = DecimalParameter(0.90, 1.5, default=sell_params['high_offset_2'], space='sell', optimize = is_optimize_sell)
 
-    # profit threshold 1, trigger point, SL_1 is used
-    pPF_1 = DecimalParameter(0.008, 0.020, default=0.016, decimals=3, space='sell', load=True)
-    pSL_1 = DecimalParameter(0.008, 0.020, default=0.011, decimals=3, space='sell', load=True)
-
-    # profit threshold 2, SL_2 is used
-    pPF_2 = DecimalParameter(0.040, 0.100, default=0.080, decimals=3, space='sell', load=True)
-    pSL_2 = DecimalParameter(0.020, 0.070, default=0.040, decimals=3, space='sell', load=True)
+    is_optimize_trailing = False
+    pPF_1 = DecimalParameter(0.011, 0.020, default=0.016, decimals=3, space='sell', load=True, optimize = is_optimize_trailing)
+    pSL_1 = DecimalParameter(0.011, 0.020, default=0.011, decimals=3, space='sell', load=True, optimize = is_optimize_trailing)
+    pPF_2 = DecimalParameter(0.040, 0.100, default=0.080, decimals=3, space='sell', load=True, optimize = is_optimize_trailing)
+    pSL_2 = DecimalParameter(0.020, 0.070, default=0.040, decimals=3, space='sell', load=True, optimize = is_optimize_trailing)
 
     def informative_pairs(self):
         pairs = self.dp.current_whitelist()
@@ -163,6 +169,28 @@ class ClucHAnix_hhll(IStrategy):
             return -0.99
 
         return stoploss_from_open(sl_profit, current_profit)
+
+    ## Confirm Entry
+    def confirm_trade_entry(self, pair: str, order_type: str, amount: float, rate: float, time_in_force: str, **kwargs) -> bool:
+
+        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+
+        max_slip = self.max_slip.value
+
+        if(len(dataframe) < 1):
+            return False
+
+        dataframe = dataframe.iloc[-1].squeeze()
+        if ((rate > dataframe['close'])) :
+
+            slippage = ( (rate / dataframe['close']) - 1 ) * 100
+
+            if slippage < max_slip:
+                return True
+            else:
+                return False
+
+        return True
 
     def custom_sell(self, pair: str, trade: 'Trade', current_time: 'datetime', current_rate: float,
                     current_profit: float, **kwargs):
